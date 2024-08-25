858a3981e62687b06ea0f19063c1113ebbbd1f29
elyasprba <elyasprba@Ts-MacBook-Pro.local>
2024-08-25 21:05:21 +0700

    chore: setup logger morgan

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import logger from 'morgan';
 
 import bestRouter from './routes';
 
@@ -9,12 +10,30 @@ dotenv.config();
 const server = express();
 const PORT = process.env.PORT || 8000;
 
-server.use(cors());
-server.use(express.urlencoded({ extended: false }));
-server.use(express.json());
+const corsOptions = {
+  origin: ['http://localhost:3000'],
+  methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS', 'PUT'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
 
-server.use(bestRouter);
+const app = async () => {
+  try {
+    server.use(cors(corsOptions));
+    server.use(express.urlencoded({ extended: false }));
+    server.use(express.json());
 
-server.listen(PORT, () => {
-  console.log(`App listening on port 8000`);
-});
+    server.use(bestRouter);
+
+    server.use(
+      logger(':method :url :status :res[content-length] - :response-time ms')
+    );
+
+    server.listen(PORT, () => {
+      console.log(`App listening on port 8000`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+app();
